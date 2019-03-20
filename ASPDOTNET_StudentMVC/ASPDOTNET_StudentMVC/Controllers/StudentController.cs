3ce581ef0c851ea395cb5024319c87c77aa89cf7
Matthew Shaw <mshaw@sparta.com>
2019-03-20 09:30:49 +0000

    code first homework

---
+++ ASPDOTNET_StudentMVC/ASPDOTNET_StudentMVC/Controllers/StudentController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASPDOTNET_StudentMVC.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            return View();
+        }
+        //About
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
---
+++ ASPDOTNET_StudentMVC/ASPDOTNET_StudentMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ASPDOTNET_StudentMVC.MvcApplication" Language="C#" %>
